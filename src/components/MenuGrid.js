8f1747384a2135254f2e7d9efdab07ce7129fda9
RaindropsZhen <wuzhenze@hotmail.com>
2025-02-06 17:34:20 -0100

    check: working version but not cnetered the menu

--- src/components/MenuGrid.js
+++ src/components/MenuGrid.js
@@ -6,9 +6,9 @@ const MenuGrid = ({ category, selectedLanguage, shoppingCart, onOrder, onRemove,
   const items = category.menu_items.filter((item) => item.is_available);
 return (
 // Modify the Row and Col to display 2 items per row on larger screens
-<Row xs={2} sm={2} md={2} lg={2} xl={2} className="g-4">
+<Row xs={2} sm={2} md={2} lg={2} xl={2} className="g-4 justify-content-center">
   {items.map((item, index) => (
-    <Col key={index} className="custom-col">
+    <Col key={index} className="custom-col" style={{ width: '50%' }}>
       <MenuItemCard
         language={selectedLanguage}
         item={{
